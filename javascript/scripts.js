aee52fecb12bd184157b1dcbf56179696ca6e90f
Eriana Gonzalez <40642695+ErianaG@users.noreply.github.com>
2018-08-15 20:30:40 -0400

    changes error messages in scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -7,35 +7,35 @@ function validate()
   
     if (name.value == "")                                 
     {
-        alert("Please enter your name.");
+        alert("Please put in your name.");
         name.focus();
         return false;
     }
       
     if (email.value == "")                                  
     {
-        alert("Please enter a valid e-mail address.");
+        alert("Please put in a valid e-mail address.");
         email.focus();
         return false;
     }
   
     if (email.value.indexOf("@", 0) < 0)                
     {
-        alert("Please enter a valid e-mail address.");
+        alert("Please put in a valid e-mail address.");
         email.focus();
         return false;
     }
   
     if (email.value.indexOf(".", 0) < 0)                
     {
-        alert("Please enter a valid e-mail address.");
+        alert("Please put in a valid e-mail address.");
         email.focus();
         return false;
     }
   
     if (phone.value == "")                          
     {
-        alert("Please enter your phone number.");
+        alert("Please put in your phone number.");
         phone.focus();
         return false;
     }
